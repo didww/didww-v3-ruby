898ab36da98f98386108ed5e698df3ec87668bf5
Alex Korobeinikov <alex.k@didww.com>
2018-02-23 14:36:02 +0200

    Make ComplexObject's handle arbitrary attributes

---
+++ spec/didww/complex_objects/base_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe DIDWW::ComplexObject::Base do
+  class self::TestComplexObject < described_class
+    property :known, type: :integer
+  end
+
+  describe 'any complex object' do
+    let(:test_class)    { self.class::TestComplexObject }
+    let(:test_instance) { test_class.new(known: '42', unknown: 'bar') }
+
+    it 'can have arbitrary properties' do
+      expect{ test_instance }.not_to raise_error
+      expect( test_instance[:unknown] ).to eq('bar')
+    end
+
+    it 'uses type casting for defined properties' do
+      expect( test_instance.known ).to be_kind_of(Fixnum)
+    end
+  end
+end
