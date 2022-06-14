6f90870fcdb82bffc955d190bb9d99e924e2aa95
Ivanov-Anton <jinsike123@gmail.com>
2022-06-14 14:55:17 +0300

    Filter available_dids resource by nanpa_prefix
    Filter nanpa_prefixes by nanpa_prefix
    request example
    ```
    GET /available_dids?filter[nanpa_prefix.id]={id}
    ```
    Using Ruby SDK:
    ```
    client.available_dids.where('nanpa_prefix.id': nanpa_prefix_id).all
    client.available_dids.includes(:did_group).find(id)
    ```

--- spec/didww/resources/available_did_spec.rb
+++ spec/didww/resources/available_did_spec.rb
@@ -61,6 +61,31 @@ RSpec.describe DIDWW::Resource::AvailableDid do
       end
     end
 
+    context 'with include nanpa_prefix' do
+      subject do
+        client.available_dids.includes(:nanpa_prefix).find(id)
+      end
+
+      let!(:mock_get_record_request) do
+        stub_didww_request(:get, "/available_dids/#{id}?include=nanpa_prefix").to_return(
+          status: 200,
+          body: api_fixture('available_dids/get/include_nanpa_prefix/200'),
+          headers: json_api_headers
+        )
+      end
+
+      it 'should fetch NanpaPrefix resource' do
+        subject
+        expect(available_did.nanpa_prefix).to be_kind_of(DIDWW::Resource::NanpaPrefix)
+        expect(available_did.nanpa_prefix).to have_attributes npa: be_kind_of(String), nxx: be_kind_of(String)
+      end
+
+      it 'request should be performed properly' do
+        subject
+        expect(mock_get_record_request).to have_been_requested.at_least_once
+      end
+    end
+
     context 'with include did_group.stock_keeping_units' do
       subject do
         client.available_dids.includes(:'did_group.stock_keeping_units').find(id)
@@ -119,6 +144,32 @@ RSpec.describe DIDWW::Resource::AvailableDid do
       end
     end
 
+    describe 'GET /available_dids?filter[nanpa_prefix.id]={id}' do
+      subject { client.available_dids.where('nanpa_prefix.id': nanpa_prefix_id).all }
+
+      let(:nanpa_prefix_id) { SecureRandom.uuid }
+
+      context 'when Available DID within NANPA prefix is exists' do
+        let!(:mock_get_available_dids_request) do
+          stub_didww_request(:get, "/available_dids?filter[nanpa_prefix.id]=#{nanpa_prefix_id}").to_return(
+            status: 200,
+            body: api_fixture('available_dids/get/sample_1/200'),
+            headers: json_api_headers
+          )
+        end
+
+        it 'returns a collection of Available Dids' do
+          expect(subject).to be_a_list_of(DIDWW::Resource::AvailableDid)
+          expect(subject.count).to eq 2
+          expect(subject.sample.type).to eq described_class.type
+        end
+
+        it 'request should be performed properly' do
+          subject
+          expect(mock_get_available_dids_request).to have_been_requested.at_least_once
+        end
+      end
+    end
   end
 
 end
