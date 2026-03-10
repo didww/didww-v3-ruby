1a2a8b0b0566478a4f8452d61bccf3b625be87cb
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-10 22:06:01 +0100

    Add hardcoded cross-SDK test vectors for RequestValidator
    Add 5 test cases with fixed API keys and expected signatures matching
    PHP, Python, Java, and .NET SDKs to catch interop regressions.

--- spec/didww/callback_request_validator_spec.rb
+++ spec/didww/callback_request_validator_spec.rb
@@ -53,4 +53,82 @@ RSpec.describe DIDWW::Callback::RequestValidator, '#validate' do
     it { is_expected.to eq(false) }
   end
 
+  context 'with cross-SDK test vectors' do
+    context 'sandbox callback' do
+      let(:api_key) { 'SOMEAPIKEY' }
+      let(:url) { 'http://example.com/callback.php?id=7ae7c48f-d48a-499f-9dc1-c9217014b457&reject_reason=&status=approved&type=address_verifications' }
+      let(:payload) do
+        {
+          'status' => 'approved',
+          'id' => '7ae7c48f-d48a-499f-9dc1-c9217014b457',
+          'type' => 'address_verifications',
+          'reject_reason' => ''
+        }
+      end
+      let(:signature) { '18050028b6b22d0ed516706fba1c1af8d6a8f9d5' }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'valid request' do
+      let(:api_key) { 'SOMEAPIKEY' }
+      let(:url) { 'http://example.com/callbacks' }
+      let(:payload) do
+        {
+          'status' => 'completed',
+          'id' => '1dd7a68b-e235-402b-8912-fe73ee14243a',
+          'type' => 'orders'
+        }
+      end
+      let(:signature) { 'fe99e416c3547f2f59002403ec856ea386d05b2f' }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'valid request with query and fragment' do
+      let(:api_key) { 'OTHERAPIKEY' }
+      let(:url) { 'http://example.com/callbacks?foo=bar#baz' }
+      let(:payload) do
+        {
+          'status' => 'completed',
+          'id' => '1dd7a68b-e235-402b-8912-fe73ee14243a',
+          'type' => 'orders'
+        }
+      end
+      let(:signature) { '32754ba93ac1207e540c0cf90371e7786b3b1cde' }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'empty signature' do
+      let(:api_key) { 'SOMEAPIKEY' }
+      let(:url) { 'http://example.com/callbacks' }
+      let(:payload) do
+        {
+          'status' => 'completed',
+          'id' => '1dd7a68b-e235-402b-8912-fe73ee14243a',
+          'type' => 'orders'
+        }
+      end
+      let(:signature) { '' }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context 'invalid signature' do
+      let(:api_key) { 'SOMEAPIKEY' }
+      let(:url) { 'http://example.com/callbacks' }
+      let(:payload) do
+        {
+          'status' => 'completed',
+          'id' => '1dd7a68b-e235-402b-8912-fe73ee14243a',
+          'type' => 'orders'
+        }
+      end
+      let(:signature) { 'fbdb1d1b18aa6c08324b7d64b71fb76370690e1d' }
+
+      it { is_expected.to eq(false) }
+    end
+  end
+
 end
