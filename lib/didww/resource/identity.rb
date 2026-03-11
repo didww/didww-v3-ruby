611dc2e9f247539bccc8daf06ad5864bc73328fa
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-11 17:05:12 +0100

    Address review feedback on DidGroup requirement relationship
    - Add requirement relationship to all did_group objects in collection fixture
    - Simplify respond_to spec to use described_class.new instead of HTTP stub
    - Remove unused IDENTITY_TYPE_ANY constant from Identity resource

--- lib/didww/resource/identity.rb
+++ lib/didww/resource/identity.rb
@@ -4,7 +4,6 @@ module DIDWW
     class Identity < Base
       IDENTITY_TYPE_PERSONAL = 'Personal'
       IDENTITY_TYPE_BUSINESS = 'Business'
-      IDENTITY_TYPE_ANY = 'Any'
 
       has_one :country, class_name: 'Country'
       has_many :proofs, class_name: 'Proof'
