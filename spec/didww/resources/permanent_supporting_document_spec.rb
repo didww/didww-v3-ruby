5477fa76f9100105c345c59083a6bce3b9e5ac94
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-16 16:21:11 +0200

    feat: add external_reference_id to PermanentSupportingDocument for 2026-04-16

--- spec/didww/resources/permanent_supporting_document_spec.rb
+++ spec/didww/resources/permanent_supporting_document_spec.rb
@@ -30,5 +30,9 @@ RSpec.describe DIDWW::Resource::PermanentSupportingDocument do
     it '"created_at", type: Time' do
       expect(document.created_at).to be_kind_of(Time)
     end
+
+    it '"external_reference_id", type: String' do
+      expect(document.external_reference_id).to be_kind_of(String).or be_nil
+    end
   end
 end
