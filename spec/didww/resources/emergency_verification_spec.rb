202a5ddccb9f1884b8a6cf40c7c93475b3ec9280
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-16 16:19:48 +0200

    feat: support PATCH /emergency_verifications/:id to update external_reference_id (2026-04-16)

--- spec/didww/resources/emergency_verification_spec.rb
+++ spec/didww/resources/emergency_verification_spec.rb
@@ -86,6 +86,30 @@ RSpec.describe DIDWW::Resource::EmergencyVerification do
     end
   end
 
+  describe 'PATCH /emergency_verifications/:id' do
+    let(:id) { '01234567-89ab-cdef-0123-456789abcdef' }
+
+    it 'updates external_reference_id' do
+      stub_didww_request(:patch, "/emergency_verifications/#{id}").with(
+        body: {
+          data: {
+            id: id,
+            type: 'emergency_verifications',
+            attributes: { external_reference_id: 'updated-ev-ref-77' }
+          }
+        }.to_json
+      ).to_return(
+        status: 200,
+        body: api_fixture('emergency_verifications/id/patch/update_external_reference_id/200'),
+        headers: json_api_headers
+      )
+      verification = described_class.load(id: id)
+      verification.external_reference_id = 'updated-ev-ref-77'
+      expect(verification.save).to eq(true)
+      expect(verification.external_reference_id).to eq('updated-ev-ref-77')
+    end
+  end
+
   it 'exposes canonical STATUSES' do
     expect(described_class::STATUSES).to eq(%w[pending approved rejected])
   end
